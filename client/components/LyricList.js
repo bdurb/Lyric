794974bc833416117637afe57b320f1636f4efd6
bdurb <brian@rpmgreenville.com>
2018-11-09 18:14:13 -0800

    set up likes button to upvote lyrics, and made a change to the song query to include likes from lyrics

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
 
+  onLike(id) {
+    this.props.mutate({ variables: { id } })
+  }
+
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className="collection-item">
           {content}
+          <div className="vote-box">
+            <i className="material-icons" onClick={() => this.onLike(id)}>thumb_up</i>
+            {likes}
+          </div>
         </li>
       )
     })
@@ -20,5 +30,14 @@ class LyricList extends Component {
     );
   }
 }
+
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`
  
-export default LyricList;
+export default graphql(mutation)(LyricList);

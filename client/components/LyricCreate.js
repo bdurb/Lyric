168dcee0bd279da59a4d71d76dcb61adecec2609
bdurb <brian@rpmgreenville.com>
2018-11-09 17:19:59 -0800

    created mutation to add lyrics to a specific song

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo';
 
 class LyricCreate extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      content: ''
+    }
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+    this.props.mutate({
+      variables: {
+        content: this.state.content,
+        songId: this.props.songId
+      }
+    })
+  }
+
   render() { 
     return (
-      <form>
+      <form onSubmit={this.onSubmit.bind(this)}>
         <label>Add a Lyric</label>
-        <input />
+        <input value={this.state.content} onChange={e => this.setState({ content: e.target.value})}/>
       </form>
     );
   }
 }
+
+const mutation = gql`
+  mutation AddLyricToSong($content: String, $songId: ID) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+      lyrics {
+        content
+      }
+    }
+  }
+`
  
-export default LyricCreate;
+export default graphql(mutation)(LyricCreate);
